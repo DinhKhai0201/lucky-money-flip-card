03e6aae0117ce3c046ff42e272557ae92863bbb7
dinhkhai0201 <nguyendinhkhai2111997@gmail.com>
2025-01-25 21:45:41 +0700

    update telegram bot

--- src/telegram.js
+++ src/telegram.js
@@ -1,7 +1,7 @@
 import { makePostRequest } from "./api";
 
-const telegramBotKey = "6944534289:AAEqaWi8yusKoTCjcJx4aTmcx6Irg3LsNHQ";
-const chat_id = '-4100329715';
+const telegramBotKey = "7574243925:AAGqDzn_Gq8pV6NDqkZCiCbRLNfB0BbrbOo";
+const chat_id = '-4637934528';
 
 export const sendNotification = async (text) => {
     const endpoint = `https://api.telegram.org/bot${telegramBotKey}/sendMessage?chat_id=${chat_id}&text=${text}`;
